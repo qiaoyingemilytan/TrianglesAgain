5f138a50bf067adf79c525318b70bf943698c409
Emily <qtan20@stuy.edu>
2020-10-29 17:44:55 -0400

    I have tested this code and it works

--- Tester.java
+++ Tester.java
@@ -1,10 +1,13 @@
 public class Tester{
   public static void main(String[] args){
     Point a = new Point(2, 3);
-    Point b = new Point(3, 3);
+    Point b = new Point(5, 3);
+    Point c = new Point(4, 6);
+    Triangle a1 = new Triangle(2, 3, 5, 5, 4, 6);
     System.out.println(a.getX());
     System.out.println(a.getY());
     System.out.println(a.distanceTo(b));
     System.out.println(a.equals(b));
+    System.out.println(a1.getPerimeter());
   }
 }
