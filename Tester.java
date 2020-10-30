14d9b4005e8fe223b6397120b8eca3719a54baba
Emily <qtan20@stuy.edu>
2020-10-29 22:42:06 -0400

    I have tested this code and it works

--- Tester.java
+++ Tester.java
@@ -3,7 +3,7 @@ public class Tester{
     Point a = new Point(2, 3);
     Point b = new Point(5, 3);
     Point c = new Point(4, 6);
-    Triangle a1 = new Triangle(2, 3, 5, 5, 4, 6);
+    Triangle a1 = new Triangle(a,b,c);
     System.out.println(a.getX());
     System.out.println(a.getY());
     System.out.println(a.distanceTo(b));
