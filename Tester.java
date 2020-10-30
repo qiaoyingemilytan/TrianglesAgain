926b840e9d0d3159923e25aa5197cfb82477030b
Emily <qtan20@stuy.edu>
2020-10-29 23:20:05 -0400

    I have tested this code and it works

--- Tester.java
+++ Tester.java
@@ -3,6 +3,7 @@ public class Tester{
     Point a = new Point(2, 3);
     Point b = new Point(5, 3);
     Point c = new Point(3.5, 6);
+    Point d = new Point(3.5, 0);
     Triangle a1 = new Triangle(a,b,c);
     System.out.println(a.getX());
     System.out.println(a.getY());
@@ -11,6 +12,8 @@ public class Tester{
     System.out.println(a1.getPerimeter());
     System.out.println(a1.getArea());
     System.out.println(a1.classify());
+    System.out.println(a1.toString());
+    a1.setVertex(2, d);
     System.out.println(a1.toString());
   }
 }
--- Triangle.java
+++ Triangle.java
@@ -37,4 +37,16 @@ public class Triangle{
     return "v1(" + v1.getX() + ", " + v1.getY() + ") v2(" + v2.getX() + ", " + v2.getY() + ") v3(" + v3.getX() + ", " + v3.getY() + ")";
   }
 
+  public void setVertex(int index, Point newP){
+    if(index == 0){
+      v1 = newP;
+    }
+    else if (index == 1){
+      v2 = newP;
+    }
+    else if (index == 2){
+      v3 = newP;
+    }
+  }
+
 }
