3ac83d0ca42da3aee0b3b1e38043d5bf4ba39a43
Emily <qtan20@stuy.edu>
2020-11-02 13:16:44 -0500

    I have tested this code and it works now

--- Tester.java
+++ Tester.java
@@ -5,13 +5,14 @@ public class Tester{
     Point c = new Point(3.5, 6);
     Point d = new Point(3.5, 0);
     Triangle a1 = new Triangle(a,b,c);
+    Triangle a2 = new Triangle(1, 2, 3 ,4, 5, 6);
     System.out.println(a.getX());
     System.out.println(a.getY());
     System.out.println(a.distanceTo(b));
     System.out.println(a.equals(b));
     System.out.println(a1.getPerimeter());
     System.out.println(a1.getArea());
-    System.out.println(a1.classify());
+    System.out.println(a2.classify());
     System.out.println(a1.toString());
     a1.setVertex(2, d);
     System.out.println(a1.toString());
