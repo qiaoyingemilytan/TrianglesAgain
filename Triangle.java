dae18096b7b1cc29a65baf87cd5391b10894fd37
Emily <qtan20@stuy.edu>
2020-11-03 00:55:53 -0500

    I have tested this code and it works now

--- Triangle.java
+++ Triangle.java
@@ -8,9 +8,9 @@ public class Triangle{
   }
 
   public Triangle(double x1, double y1, double x2, double y2,double x3, double y3){
-    Point v1 = new Point(x1, y1);
-    Point v2 = new Point(x2, y2);
-    Point v3 = new Point(x3, y3);
+    v1 = new Point(x1, y1);
+    v2 = new Point(x2, y2);
+    v3 = new Point(x3, y3);
   }
 
   public double getPerimeter(){
